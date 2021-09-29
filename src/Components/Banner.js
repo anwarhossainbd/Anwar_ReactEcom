01b8d25c50dc0db1011ac7720a32e9046ab99f36
unknown <mdanwarhossain1996bd@gmail.com>
2021-09-29 14:02:40 -0600

    Running

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Banner = () => {
     return (
-       <div className="" >
+       <div className="text-center" >
            <header>
+               <div className="headerText ">
+                   <div>
+                       <h1>We are always with You</h1>
+                       <p>Enjoy online shopping with BD Express</p>
+                   </div>
 
+               </div>
            </header>
        </div>
     );
---
+++ src/Global/ProductContext.js
@@ -0,0 +1,22 @@
+import React, {createContext} from 'react';
+
+import AppleLaptop from "../Assets/AppleLaptop.jpg";
+import Laptop2 from "../Assets/Laptop2.jpg";
+import Camera1 from "../Assets/Camera1.jpg";
+import Camera2 from "../Assets/Camera2.jpg";
+import HeadPhone1 from "../Assets/HeadPhone1.jpeg";
+import Mobile from "../Assets/Mobile.png"
+import Watch1 from "../Assets/Watch1.png"
+import Mobile from "../Assets/Mobile.png"
+
+export const ProductContext = createContext()
+
+const ProductContextProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductContextProvider;
