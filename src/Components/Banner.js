a9de51bb7ccbf9467eb04c2faf64700b0c752a56
unknown <mdanwarhossain1996bd@gmail.com>
2021-09-29 02:49:09 -0600

    React Ecommerce

---
+++ src/Components/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+       <div className="" >
+           <header>
+
+           </header>
+       </div>
+    );
+};
+
+export default Banner;
+
+
+
